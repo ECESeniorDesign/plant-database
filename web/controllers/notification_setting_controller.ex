e81bf6844e5ffeecc8293f5127b5c21a9581e400
Chase Conklin <chase@conklins.net>
2016-03-31 11:38:18 -0400

    add API route for notification settings close #2

---
+++ web/controllers/notification_setting_controller.ex
@@ -0,0 +1,17 @@
+defmodule Backend.NotificationSettingController do
+  use Backend.Web, :controller
+
+  def update(conn, %{"token" => token, "email" => email, "push" => push}) do
+    user_id = Backend.Authenticator.authenticate_token(token)
+    case user_id do
+      :invalid -> send_resp(conn, 403, "Invalid Credentials")
+      _ ->
+        user = Repo.get(User, user_id)
+          |> Repo.preload :notification_setting
+        Backend.NotificationSetting.changeset(user.notification_setting, %{"email" => email, "push" => push})
+          |> Repo.update!
+        send_resp(conn, 200, "OK")
+    end
+  end
+  
+end
