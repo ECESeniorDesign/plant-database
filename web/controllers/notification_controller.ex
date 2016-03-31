6d684d28e1e70f50bb9c70691b5445d1e83658a7
Chase Conklin <chase@conklins.net>
2016-03-30 22:31:23 -0400

    send push notifications when notifying close #2

--- web/controllers/notification_controller.ex
+++ web/controllers/notification_controller.ex
@@ -7,7 +7,11 @@ defmodule Backend.NotificationController do
       :invalid -> send_resp(conn, 403, "Invalid Credentials")
       _ ->
         user = Repo.get(User, user_id)
+          |> Repo.preload :devices
         Backend.Mailer.send_notification_email(user.email, title, message)
+        for device <- user.devices do
+          APNS.push :app1_dev_pool, device.device_id, message
+        end
         send_resp(conn, 200, "OK")
     end
   end
